ebb97b3b1434ead6b0ef2a61185c2e6418669e71
smoskii <cmyk05@gmail.com>
2024-09-07 20:48:39 -0700

    Created test script

---
+++ Assets/text.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y += 2;
+        transform.position = pos;
+    }
+}
